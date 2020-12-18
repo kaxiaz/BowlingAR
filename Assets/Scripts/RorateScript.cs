ebe9f3e98f86b874d642a3c3b53dd1ffda6f3ace
KaxiaZ <diawjbegin@gmail.com>
2020-12-18 08:39:28 +0700

    first commit

---
+++ Assets/Scripts/RorateScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RorateScript : MonoBehaviour
+{
+    public float rotateSpeed = 10;
+    public float minMaxRotationAngles = 20;
+    protected bool turnRight = true;
+    protected float min = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        min = 360 - minMaxRotationAngles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (turnRight)
+        {
+            transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
+            if (transform.rotation.eulerAngles.y > minMaxRotationAngles && transform.rotation.eulerAngles.y < min)
+            {
+                turnRight = false;
+            }
+        }
+        else
+        {
+            transform.Rotate(0, -rotateSpeed * Time.deltaTime, 0);
+            if (transform.rotation.eulerAngles.y > minMaxRotationAngles && transform.rotation.eulerAngles.y < min)
+            {
+                turnRight = true;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ShootScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootScript : MonoBehaviour
+{
+    public float ballSpeed = 2000;
+    // public float moveSpeed = 5;
+
+    // public Transform BallSpawn;
+
+    void Update()
+    {
+        // Vector3 move = Vector3.zero;
+        // move.z = Input.GetAxis("Horizontal");
+
+        // transform.position += move * moveSpeed * Time.deltaTime;
+    }
+
+    public void Shoot() {
+        GetComponent<Rigidbody>().AddForce(ballSpeed * transform.forward, ForceMode.Impulse);
+    }
+
+    // public void Reset(object _ball)
+    // {
+    //     gameObject.transform.position = BallSpawn.position;
+    //     gameObject.transform.rotation = BallSpawn.rotation;
+    //     gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+    //     gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+    // }
+
+}
